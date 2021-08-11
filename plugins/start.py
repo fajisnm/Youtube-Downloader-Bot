18be5f9645e66bf4662416672c274221959c3a48
fajisnm <87913488+fajisnm@users.noreply.github.com>
2021-08-11 14:05:56 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,6 +9,6 @@ async def start(client, message):
         InlineKeyboardButton("Updates", url="https://t.me/botcodesforyou"),
         InlineKeyboardButton("Support Group", url="https://t.me/codingdiscuss")
      ]])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nim a YouTube video downloader bot\nsend Me a YouTube video link and download it to your gallery\n\nHit /help for More info\n\ni hope you don't forget to join My channels 🥰"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
