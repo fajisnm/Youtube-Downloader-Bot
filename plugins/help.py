14204f21553b066961b888d31cd4d2a8228c219a
fajisnm <87913488+fajisnm@users.noreply.github.com>
2021-08-11 14:47:07 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"I'm A Simple YouTube Video Link Downloader Bot.\n\nPlease Send Me Any YouTube Video Link,\n\n I Can Upload It To Telegram As File, Audio Or Video Format."
+    helptxt = f"I'm a Simple YouTube Video Link Downloader Bot.\n\nPlease Send Me Any YouTube Video Link,\n\n I Can Upload It To Telegram As File, Audio Or Video Format."
     await message.reply_text(helptxt)
