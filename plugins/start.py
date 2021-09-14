57920829ec1a27d0cab98f3571f4df7d85fb908a
Joke le Sare <87913488+jokeselo@users.noreply.github.com>
2021-09-14 16:06:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -14,7 +14,7 @@ async def start(client, message):
 Fajis= "https://telegra.ph/file/f1d7b30b05ba9f0dbf4e5.jpg"
 
     
-   welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+   welcomed = f"Hey Man /help for More info"
     await message.reply_photo(Fajis,welcomed,reply_markup=joinButton)
 
 raise StopPropagation
