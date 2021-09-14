605d5ae5267716ff54c72ba2889abe23badf1691
Joke le Sare <87913488+jokeselo@users.noreply.github.com>
2021-09-14 15:57:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -14,8 +14,8 @@ async def start(client, message):
 Fajis= "https://telegra.ph/file/f1d7b30b05ba9f0dbf4e5.jpg"
 
     
-    welcom = f"Hey <b>{message.from_user.first_name}</b>\n/n I'm A Simple YouTube Video Downloader Bot With YouTube Video Thumbnail Support.\n\nPlease Send Me Any YouTube Video Link, I Can Upload It To Telegram As File, Audio Or Video Format.\n\nIf Found Any Bugs Then, You Can Report In Our Support Group !\n\nClick On /help For More Details....</b>"
-    await message.reply_photo(Fajis,welcom,reply_markup=joinButton)
+   welcomed = f"Hey <b>{message.from_user.first_name}</b>\nI'm A Simple YouTube Video Downloader Bot With YouTube Video Thumbnail Support.\n\nPlease Send Me Any YouTube Video Link, I Can Upload It To Telegram As File, Audio Or Video Format.\n\nIf Found Any Bugs Then, You Can Report In Our Support Group !\n\nClick On /help For More Details...."
+    await message.reply_photo(Fajis,welcomed,reply_markup=joinButton)
 
 raise StopPropagation
     
