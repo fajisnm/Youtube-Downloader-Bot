76ed4d67b37aa376ba37ea46404162b0813f4f99
Joke le Sare <87913488+jokeselo@users.noreply.github.com>
2021-09-14 15:19:58 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -16,4 +16,6 @@ Fajis= "https://telegra.ph/file/f1d7b30b05ba9f0dbf4e5.jpg"
     
     welcom = f"Hey <b>{message.from_user.first_name}</b>\n/ I'm A Simple YouTube Video Downloader Bot With YouTube Video Thumbnail Support.\n\nPlease Send Me Any YouTube Video Link, I Can Upload It To Telegram As File, Audio Or Video Format.\n\nIf Found Any Bugs Then, You Can Report In Our Support Group !\n\nClick On /help For More Details....</b>"
     await message.reply_photo(Fajis,welcom,reply_markup=joinButton)
+
+raise StopPropagation
     
