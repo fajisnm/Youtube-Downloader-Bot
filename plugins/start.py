c4d296b5b0ff89dfb9f1f6dc171f1431eaf6e80e
Joke le Sare <87913488+jokeselo@users.noreply.github.com>
2021-09-14 16:03:29 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -14,7 +14,7 @@ async def start(client, message):
 Fajis= "https://telegra.ph/file/f1d7b30b05ba9f0dbf4e5.jpg"
 
     
-   welcomed = f"Hey <b>{message.from_user.first_name}</b>\nI'm A Simple YouTube Video Downloader Bot With YouTube Video Thumbnail Support.\n\nPlease Send Me Any YouTube Video Link, I Can Upload It To Telegram As File, Audio Or Video Format.\n\nIf Found Any Bugs Then, You Can Report In Our Support Group !\n\nClick On /help For More Details...."
+   welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_photo(Fajis,welcomed,reply_markup=joinButton)
 
 raise StopPropagation
